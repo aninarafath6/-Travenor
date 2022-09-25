5f4550b1bc6f792da489c4425564db456daa9dc5
aninarafath6 <arafathanin6@gmail.com>
2022-09-25 13:54:31 +0530

    constants: color constants setup

--- lib/common/constants/color_constatns.dart
+++ lib/common/constants/color_constatns.dart
@@ -3,12 +3,12 @@ import 'package:flutter/painting.dart';
 abstract class AppColors {
   static const primaryColor = Color(0xff0D6EFD);
   static const subTextColor = Color(0xff707B81);
+  static const actionColor = Color(0xffFF7029);
 }
 
 class LightApp extends AppColors {
   static const textColor = Color(0xff1B1E28);
   static const boxFill = Color(0xffF7F7F9);
-  static const actionColor = Color(0xffFF7029);
   static const lineStokeColor = Color(0xffF8F9FA);
   static const bgColor = Color(0xffFFFFFF);
   static const frameColor = Color(0xffF8F9FA);
